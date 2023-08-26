d09a156c64eea02045afffb4b828ba4edf7f5a8b
Raymond Deng <1107450@gmail.com>
2023-08-25 19:51:40 -0700

    add outlinte

--- lcr/lcr.cpp
+++ lcr/lcr.cpp
@@ -4,6 +4,28 @@
 int main(){
 	int numProcesses = 15;
 
+	/*
+	upon receiving no message
+		send uid to left
+
+	upon receiving message from right
+	case:
+		m.uid > uid:
+			send m to left
+		m.uid < uid:
+			discard m
+		m.uid = uid:
+			leader := i
+			send <terminate, i> to left
+			terminate
+		endcase
+
+	upon receiving <terminate, i> from right
+		leader := i
+		send <terminate, i> to left
+		terminate
+	*/
+
 	for (int processIdx = 0; processIdx < numProcesses; processIdx++){
 		pid = fork();
 	}
