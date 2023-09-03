2cd425cf216faa23eab1d48eb0c0545f8c7474a0
Raymond Deng <1107450@gmail.com>
2023-09-02 19:13:02 -0700

    add some stuff on ring model

--- lcr/lcr.cpp
+++ lcr/lcr.cpp
@@ -2,6 +2,11 @@
 #include <cstdlib>
 #include <ctime>
 #include <vector>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <unordered_map>
 
 using namespace std;
 
@@ -39,29 +44,51 @@ int main() {
     cout << "Enter the number of processes in the ring: ";
     cin >> numProcesses;
 
-    vector<Process> processes;
+    // vector<Process> numProcesses
+    // cout << "server_addr sizeof " << sizeof(server_addr) << endl;
+
+    unordered_map<int, sockaddr_un> ringModel;
 
     // Create processes
     for (int i = 0; i < numProcesses; ++i) {
-        processes.emplace_back(i);
+        pid_t child_pid = fork();
+        if (child_pid == -1) {
+            cerr << "Fork failed!" << endl;
+            return 1;
+        }
+
+        if (child_pid == 0) {
+            cout << "I am a child!" << getpid() << endl;
+
+            struct sockaddr_un addr;
+            cout << "sockaddr_un size " << sizeof(sockaddr_un) << endl;
+
+            int sock = socket(AF_UNIX, SOCK_STREAM, 0);
+            if (sock == -1){
+            	cout << "Opening socket failed!" << endl;
+            	return 1;
+            }
+
+            return 0;
+        }
     }
 
     // Simulate message passing until a leader is elected
-    srand(time(nullptr));
-    int senderIdx = rand() % numProcesses;
-    int receiverIdx = (senderIdx + 1) % numProcesses;
+    // srand(time(nullptr));
+    // int senderIdx = rand() % numProcesses;
+    // int receiverIdx = (senderIdx + 1) % numProcesses;
 
-    while (true) {
-        processes[senderIdx].sendMessage(processes[receiverIdx]);
+    // while (true) {
+    //     processes[senderIdx].sendMessage(processes[receiverIdx]);
 
-        if (processes[receiverIdx].leader) {
-            cout << "Leader elected: Process " << receiverIdx << endl;
-            break;
-        }
+    //     if (processes[receiverIdx].leader) {
+    //         cout << "Leader elected: Process " << receiverIdx << endl;
+    //         break;
+    //     }
 
-        senderIdx = receiverIdx;
-        receiverIdx = (receiverIdx + 1) % numProcesses;
-    }
+    //     senderIdx = receiverIdx;
+    //     receiverIdx = (receiverIdx + 1) % numProcesses;
+    // }
 
     return 0;
 }
