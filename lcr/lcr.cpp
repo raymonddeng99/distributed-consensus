18c7bf82b134154a7b00e5b3b9c8386a8de70b83
Raymond Deng <1107450@gmail.com>
2023-09-03 16:37:51 -0700

    add stuff and things

--- lcr/lcr.cpp
+++ lcr/lcr.cpp
@@ -1,14 +1,10 @@
 #include <iostream>
-#include <cstdlib>
-#include <ctime>
-#include <vector>
 #include <unistd.h>
-#include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/un.h>
 #include <unordered_map>
 
-using namespace std;
+#define INIT_SOCK_PATH "unix_sock.0"
 
 class Process {
 public:
@@ -16,20 +12,82 @@ public:
     bool leader;
     bool active;
 
-    Process(int _id) : id(_id), leader(false), active(true) {}
+    Process(int _id, const char* SOCK_PATH) : id(_id), leader(false), active(true) {
+        struct sockaddr_un node_addr;
+        memset(&node_addr, 0, sizeof(node_addr));
+
+        int node_sock = socket(AF_UNIX, SOCK_STREAM, 0);
+        if (node_sock == -1){
+            printf("Opening socket failed!\n");
+            exit(1);
+        }
+
+        node_addr.sun_family = AF_UNIX;
+        strcpy(node_addr.sun_path, SOCK_PATH);
+        int len = sizeof(node_addr);
+        unlink(SOCK_PATH);
+
+        int rc;
+
+        rc = bind(node_sock, (struct sockaddr *)&node_addr, len);
+        if (rc == -1){
+            printf("NODE %d: Node bind error: %s\n", _id, strerror(errno));
+            close(node_sock);
+            exit(1);
+        }
+
+        rc = listen(node_sock, 1);
+        if (rc == -1){
+            printf("NODE %d: Listen error: %s\n", _id, strerror(errno));
+            close(node_sock);
+            exit(1);
+        }
+
+        printf("NODE %d listening...\n", _id);
+
+        struct sockaddr_un client_addr;
+        memset(&client_addr, 0, sizeof(client_addr));
+
+        int client_fd = accept(node_sock, (struct sockaddr *) &client_addr, (socklen_t*)&len);
+        if (client_fd == -1){
+            printf("NODE %d: Accept error: %s\n", _id, strerror(errno));
+            close(node_sock);
+            close(client_fd);
+            exit(1);
+        }
+
+        printf("NODE %d: Connected to client at: %s\n", _id, client_addr.sun_path);
+        printf("NODE %d: Waiting for message...\n", _id);
+
+        char buf[256];
+        memset(buf, 0, 256);
+
+        // while(1){
+            int byte_recv = recv(client_fd, buf, sizeof(buf), 0);
+            if (byte_recv == -1){
+                printf("NODE %d: Error when receiving message: %s\n", _id, strerror(errno));
+                close(node_sock);
+                close(client_fd);
+                exit(1);
+            }
+            else {
+                printf("NODE %d: Node received message: %s\n", _id, buf);
+            }
+        // }
+    }
 
     void sendMessage(Process& receiver) {
-        cout << "Process " << id << " sends a message to Process " << receiver.id << endl;
+        std::cout << "Process " << id << " sends a message to Process " << receiver.id << std::endl;
         receiver.receiveMessage(*this);
     }
 
     void receiveMessage(Process& sender) {
         if (active) {
-            cout << "Process " << id << " received a message from Process " << sender.id << endl;
+            std::cout << "Process " << id << " received a message from Process " << sender.id << std::endl;
 
             if (id > sender.id) {
                 leader = true;
-                cout << "Process " << id << " declares itself as the leader." << endl;
+                std::cout << "Process " << id << " declares itself as the leader." << std::endl;
             }
             else {
                 leader = false;
@@ -41,54 +99,54 @@ public:
 
 int main() {
     int numProcesses;
-    cout << "Enter the number of processes in the ring: ";
-    cin >> numProcesses;
+    std::cout << "Enter the number of processes in the ring: ";
+    std::cin >> numProcesses;
 
-    // vector<Process> numProcesses
-    // cout << "server_addr sizeof " << sizeof(server_addr) << endl;
-
-    unordered_map<int, sockaddr_un> ringModel;
+    std::unordered_map<int, sockaddr_un> ringModel;
 
     // Create processes
-    for (int i = 0; i < numProcesses; ++i) {
-        pid_t child_pid = fork();
-        if (child_pid == -1) {
-            cerr << "Fork failed!" << endl;
-            return 1;
+    for (int ringIdx = 0; ringIdx < numProcesses; ++ringIdx) {
+        pid_t node_pid = fork();
+        if (node_pid == -1) {
+            std::cerr << "Fork failed!" << std::endl;
+            exit(1);
         }
 
-        if (child_pid == 0) {
-            cout << "I am a child!" << getpid() << endl;
-
-            struct sockaddr_un addr;
-            cout << "sockaddr_un size " << sizeof(sockaddr_un) << endl;
-
-            int sock = socket(AF_UNIX, SOCK_STREAM, 0);
-            if (sock == -1){
-            	cout << "Opening socket failed!" << endl;
-            	return 1;
-            }
-
-            return 0;
+        if (node_pid == 0) {
+            std::string SOCK_PATH_STRING = "unix_sock." + std::to_string(ringIdx);
+            const char* SOCK_PATH = SOCK_PATH_STRING.c_str();
+            Process node(ringIdx, SOCK_PATH);
+            std::cout << "Created process with SOCK_PATH: " << SOCK_PATH << std::endl;
+            exit(0);
         }
     }
 
-    // Simulate message passing until a leader is elected
-    // srand(time(nullptr));
-    // int senderIdx = rand() % numProcesses;
-    // int receiverIdx = (senderIdx + 1) % numProcesses;
+    int coordinator_sock = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (coordinator_sock == -1) {
+        printf("COORDINATOR: Socket error: %s\n", strerror(errno));
+        exit(1);
+    }
 
-    // while (true) {
-    //     processes[senderIdx].sendMessage(processes[receiverIdx]);
+    struct sockaddr_un node_addr;
+    memset(&node_addr, 0, sizeof(node_addr));
+    node_addr.sun_family = AF_UNIX;
+    strcpy(node_addr.sun_path, INIT_SOCK_PATH);
 
-    //     if (processes[receiverIdx].leader) {
-    //         cout << "Leader elected: Process " << receiverIdx << endl;
-    //         break;
-    //     }
+    int rc = connect(coordinator_sock, (struct sockaddr*)&node_addr, sizeof(node_addr));
+    if(rc == -1)
+    {
+        printf("COORDINATOR: Connect error. %s\n", strerror(errno));
+        close(coordinator_sock);
+        exit(1);
+    }
 
-    //     senderIdx = receiverIdx;
-    //     receiverIdx = (receiverIdx + 1) % numProcesses;
-    // }
+    printf("COORDINATOR: Connected to node %s\n", INIT_SOCK_PATH);
+
+    for (int ringIdx = 0; ringIdx < numProcesses; ++ringIdx) {
+        std::string SOCK_PATH_STRING = "unix_sock." + std::to_string(ringIdx);
+        const char* SOCK_PATH = SOCK_PATH_STRING.c_str();
+        remove(SOCK_PATH);
+    }
 
     return 0;
 }
