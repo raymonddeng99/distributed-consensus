bfc6daf9816d5f3bbc5a6e7c237ecf1ca8f38a6c
Raymond Deng <1107450@gmail.com>
2024-02-08 17:48:53 -0800

    update with helper functions

--- raft/src/server.h
+++ raft/src/server.h
@@ -1,32 +1,50 @@
 #ifndef _SERVER_H
 #define _SERVER_H
 
-class RaftServerImpl final: public RaftProtocol::Server {
+#include "configurator.h"
+#include "Raft.capnp.h"
+#include <capnp/rpc.h>
+#include <capnp/serialize.h>
+#include <iostream>
+#include <chrono>
+#include <cmath>
+#include <vector>
+
+class RaftServerImpl final: public Raft::Server {
 public:
 	explicit RaftServerImpl(const std::string& nodeId);
-    kj::Promise<NodeInfo::Reader> appendEntry(FindSuccessorContext context) override;
-    kj::Promise<NodeInfo::Reader> requestVote(FindPredecessorContext context) override;
-
-    kj::Promise<NodeInfo::Reader> handleAppendEntry(HandleAppendEntryContext context) override;
-    kj::Promise<NodeInfo::Reader> handleRequestVote(HandleRequestVoteContext context) override;
+    kj::Promise<void> appendEntry(AppendEntryContext context) override;
+    kj::Promise<void> requestVote(RequestVoteContext context) override;
+    kj::Promise<void> clientRequest(ClientRequestContext context) override;
 
 private:
+	void setElectionTimer();
+    void handleElectionTimeout();
+    void convertToCandidate();
+    void requestVotes();
+    void convertToLeader();
+    void leaderHeartbeat();
+
+	NodeConfig nodeConfig;
+    std::unordered_map<uuid_t, Raft::Client> peers;
+    std::unordered_map<NodeConfig> clusterConfig;
+
 	int currentTerm;
 	std::string votedFor;
-	vector<std::string> log;
+	vector<Entry>log;
 
 	int commitIndex;
 	int lastIndex;
 
-	int nextIndex;
-	int matchIndex;
+	std::unordered_map<uuid_t, int> nextIndex;
+    std::unordered_map<uuid_t, int> matchIndex;
+    std::chrono::system_clock::time_point lastHeartbeatTime;
 
 	enum class ServerState{
 		FOLLOWER,
 		CANDIDATE,
 		LEADER
-	}
-	ServerState currentState;
+	} currentState;
 }
 
 #endif
